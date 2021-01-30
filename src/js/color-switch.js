d487b1ac0289c9a68cb694d4f8e7d3d8a11d44e8
Sergey Glazyrin <glazyrinsb@gmail.com>
2021-01-30 21:45:45 +0200

    Update color-switch.js

--- src/js/color-switch.js
+++ src/js/color-switch.js
@@ -1,12 +1,33 @@
-const colors = [
-  '#FFFFFF',
-  '#2196F3',
-  '#4CAF50',
-  '#FF9800',
-  '#009688',
-  '#795548',
-];
+import colors from './colors';
 
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+const ref = {
+  colorBody: document.querySelector('body'),
+  startButton: document.querySelector('button[data-action="start"]'),
+  stopButton: document.querySelector('button[data-action="stop"]'),
 };
+
+ref.startButton.addEventListener('click', startChangingBackgroundColor);
+ref.stopButton.addEventListener('click', stopChangingBackgroundColor);
+
+let intervalId = null;
+
+function startChangingBackgroundColor() {
+  intervalId = setInterval(() => {
+    const randomIntegerFromInterval = (min, max) => {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+
+    const indexColor = randomIntegerFromInterval(0, colors.length - 1);
+
+    ref.colorBody.style.backgroundColor = colors[indexColor];
+  }, 1000);
+
+  ref.startButton.disabled = true;
+}
+
+function stopChangingBackgroundColor() {
+  clearInterval(intervalId);
+
+  intervalId = null;
+  ref.startButton.disabled = false;
+}
